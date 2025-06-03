fdca9641f49a8fa011ce0d06a985088713b7ad75
sanzida822 <sanzidasultana822@gmail.com>
2025-06-03 10:26:21 +0600

    initial commit

---
+++ springMvc/src/main/java/com/example/springmvc/controller/AuthenticationController.java
@@ -0,0 +1,12 @@
+package com.example.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthenticationController {
+@RequestMapping("/login")
+    public String login() {
+    return "login";
+}
+}
